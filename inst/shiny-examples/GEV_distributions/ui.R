95eb16c39e9f1f3da6505b7add153411acfd962e
Antoine Pissoort <antoine.pissoort@student.uclouvain.be>
2017-08-01 23:54:02 +0200

    add tests gif

--- inst/shiny-examples/GEV_distributions/ui.R
+++ inst/shiny-examples/GEV_distributions/ui.R
@@ -10,10 +10,10 @@ shinyUI(fluidPage(
   fluidRow(
     column(3,
            numericInput("ksi1", "Shape parameter for Weibull-type",
-                        "-0.5", min = "-100", max = "0", step = 1 ),
+                        "-0.5", min = "-100", max = "0", step = .1 ),
            #numericInput("ksi2", "Which value of shape for 2nd density","0", min = "-100", max = "100" ),
            numericInput("ksi3", "Shape parameter for Fréchet-type",
-                        "0.5", min = "0", max = "100", step = 1 )
+                        "0.5", min = "0", max = "100", step = .1 )
     ),
     width = "100px",
 
@@ -21,7 +21,7 @@ shinyUI(fluidPage(
            numericInput("mu", "Which location parameter ?",
                         "0", min = "-100000", max = "100000" ),
            numericInput("sig", "Which scale parameter ?",
-                        "1", min = "0", max = "10000" )
+                        "1", min = "0", max = "10000", step = 0.5)
     ),
 
     # Show a plot of the generated distribution
